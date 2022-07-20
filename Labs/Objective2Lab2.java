6ed688b1e321553b739facc4ae5ca1afadd91e07
Cullen Rafferty <cullen1882@gmail.com>
2022-07-20 12:36:25 -0400

    final Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Cullen Rafferty";
+    int age = 40;
+    String state = "New York";
+    System.out.println("Name: " + name + " " + "Age: " + age + " " + "State: " + state);
+  }
+}
