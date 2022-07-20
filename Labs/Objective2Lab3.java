fcb50bcefc63887822db7c930743300a4d4750e7
Cullen Rafferty <cullen1882@gmail.com>
2022-07-20 13:42:03 -0400

    final Obj2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int YearOfBirth = 1982;
+    String MonthOfBirth = "May";
+    boolean TodayWeekend = false;
+    System.out.println(YearOfBirth);
+    System.out.println(MonthOfBirth);
+    System.out.println(TodayWeekend);
+
+  }
+}
